a823cc46d0d66449e660bf562a218566f9a03b17
Giordano Dìaz <diazgiordano@gmail.com>
2020-08-24 12:16:27 -0500

    linters set up and finish code to get info from users

---
+++ bin/main.rb
@@ -0,0 +1,11 @@
+puts " Hello and welcome to your new PLAY."
+puts "Please player 1 enter your nickname: "
+player_one = gets.chomp
+puts "Nickname's player 1 is #{player_one} "
+puts "Please player 2 enter your nickname: "
+player_two = gets.chomp
+puts "Nickname's player 2 is #{player_two} "
+puts "Player 1 choose your ITEM, write: X or O"
+item_one = gets.chomp
+item_two = []
+puts "Player 1 has #{item_one} and Player 2 has #{item_two}"
