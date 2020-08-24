b8b328d30138a2843ab483e61417496ac3a25ee3
Sanad <s_wj_88@hotmail.com>
2020-08-24 20:25:53 +0300

    Add comment lines

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,17 @@
-puts " Hello and welcome to your new PLAY."
+
+# Welcome the player to the game
+puts " Hello and welcome to Tic Tac Toe Game."
+# Ask the first player for nikename
 puts "Please player 1 enter your nickname: "
+# save player one name
 player_one = gets.chomp
+# print out player one name
 puts "Nickname's player 1 is #{player_one} "
+# Ask the second player nickname
 puts "Please player 2 enter your nickname: "
+# save player two nickname
 player_two = gets.chomp
+# print out
 puts "Nickname's player 2 is #{player_two} "
 puts "Player 1 choose your ITEM, write: X or O"
 item_one = gets.chomp
