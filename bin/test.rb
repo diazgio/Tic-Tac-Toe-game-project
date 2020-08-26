2532871c17a321822abb8db5b12174971b278b9b
Giordano Dìaz <diazgiordano@gmail.com>
2020-08-26 17:05:19 -0500

    win methods still not working

--- bin/test.rb
+++ bin/test.rb
@@ -12,23 +12,28 @@ class Board
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
+  WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [6, 4, 2],
+  [0, 4, 8]
+]
+
   def win?
-    @win = false
-    @win = true if board[0] == board[1] && board[1] == board[2]
-    @win = true if board[0] == board[4] && board[4] == board[8]
-    @win = true if board[0] == board[3] && board[3] == board[6]
-    @win = true if board[1] == board[4] && board[4] == board[7]
-    @win = true if board[2] == board[4] && board[4] == board[6]
-    @win = true if board[2] == board[5] && board[5] == board[8]
-    @win = true if board[3] == board[4] && board[4] == board[5]
-    @win = true if board[6] == board[7] && board[7] == board[8]
-    return win
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]]
+    end
   end
 
 end
 
 class Player
-  attr_accessor :token
+  attr_accessor :token, :name
   def initialize(name, token)
     @name = name
     @token = token
@@ -39,12 +44,13 @@ class Player
   end
 
   def get_choice(display)
-    puts "Please enter a number between 1 to 9: "
-    choice = gets.chomp.to_i
-    @input = choice
     loop do
+      puts "Please enter a number between 1 to 9: "
+      choice = gets.chomp.to_i
+      index = choice - 1
       if choice.between?(1,9)
-        return choice
+        return index
+        break
       else
         puts "Wrong number, please choose anthor number"
       end
@@ -57,6 +63,7 @@ class Logic
   def self.turn(display, player, player_choice)
     display.board[player_choice] = player.token
     display.display_board
+    display.win?
   end
 
 end
@@ -72,25 +79,28 @@ name_two = gets.chomp
 player_two = Player.new(name_two, 'O')
 puts "Second player name: #{name_two}, your token is 'O' "
 
-display = Board.new(Array.new(9, " "))
-display.display_board
+win_lose = false
+loop do
+  display = Board.new(Array.new(9, " "))
+  display.display_board
 
-for i in 1..9
-  if i % 2 != 0
-    player_choice = player_one.get_choice(display)
-    current = Logic.turn(display, player_one, player_choice)
-    if current == true
-      win_lose = true
-      puts "\nCongratulations, #{Player1.name}. You win!"
-      break
-    end
-  else
-    player_choice = player_two.get_choice(display)
-    current = Logic.turn(display, player_two, player_choice)
-    if current == true
-      win_lose = true
-      puts "\nCongratulations, #{Player1.name}. You win!"
-      break
+  for i in 1..9
+    if i % 2 != 0
+      player_choice = player_one.get_choice(display)
+      current = Logic.turn(display, player_one, player_choice)
+      if current == true
+        win_lose = true
+        puts "\nCongratulations, #{player_one.name}. You win!"
+        break
+      end
+    else
+      player_choice = player_two.get_choice(display)
+      current = Logic.turn(display, player_two, player_choice)
+      if current == true
+        win_lose = true
+        puts "\nCongratulations, #{player_two.name}. You win!"
+        break
+      end
     end
   end
 end
