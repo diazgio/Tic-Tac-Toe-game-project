38e1df4b0ee033b9d94331b10383deb4280d5ff0
Giordano Dìaz <diazgiordano@gmail.com>
2020-08-28 11:46:52 -0500

    Changes required fixed

--- lib/board.rb
+++ lib/board.rb
@@ -5,20 +5,15 @@
 # rubocop: disable Metrics/PerceivedComplexity
 
 class Board
-  attr_accessor :board
-  def initialize(board)
-    @board = board
+  def display_board(board)
+    "    #{board[0]} | #{board[1]} | #{board[2]}
+    ..|...|..
+    #{board[3]} | #{board[4]} | #{board[5]}
+    ..|...|..
+    #{board[6]} | #{board[7]} | #{board[8]}"
   end
 
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ' ----------- '
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts ' ----------- '
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
-  def win?
+  def win?(board)
     @win = false
     @win = true if board[0] == board[1] && board[1] == board[2]
     @win = true if board[0] == board[4] && board[4] == board[8]
