c91b68f6abc2632e35782a8256e75891ceaf2d32
Sanad <s_wj_88@hotmail.com>
2020-08-28 22:23:55 +0300

    fix changes

--- lib/board.rb
+++ lib/board.rb
@@ -3,24 +3,29 @@
 # rubocop: disable Metrics/PerceivedComplexity
 
 class Board
-  def display_board(board)
-    "    #{board[0]} | #{board[1]} | #{board[2]}
-    ..|...|..
-    #{board[3]} | #{board[4]} | #{board[5]}
-    ..|...|..
-    #{board[6]} | #{board[7]} | #{board[8]}"
+  attr_accessor :board
+  def initialize
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  def win?(board)
+  def display_board
+    "    #{@board[0]} | #{@board[1]} | #{@board[2]}
+    ..|...|..
+    #{@board[3]} | #{@board[4]} | #{@board[5]}
+    ..|...|..
+    #{@board[6]} | #{@board[7]} | #{@board[8]}"
+  end
+
+  def win?
     @win = false
-    @win = true if board[0] == board[1] && board[1] == board[2]
-    @win = true if board[0] == board[4] && board[4] == board[8]
-    @win = true if board[0] == board[3] && board[3] == board[6]
-    @win = true if board[1] == board[4] && board[4] == board[7]
-    @win = true if board[2] == board[4] && board[4] == board[6]
-    @win = true if board[2] == board[5] && board[5] == board[8]
-    @win = true if board[3] == board[4] && board[4] == board[5]
-    @win = true if board[6] == board[7] && board[7] == board[8]
+    @win = true if @board[0] == @board[1] && @board[1] == @board[2]
+    @win = true if @board[0] == @board[4] && @board[4] == @board[8]
+    @win = true if @board[0] == @board[3] && @board[3] == @board[6]
+    @win = true if @board[1] == @board[4] && @board[4] == @board[7]
+    @win = true if @board[2] == @board[4] && @board[4] == @board[6]
+    @win = true if @board[2] == @board[5] && @board[5] == @board[8]
+    @win = true if @board[3] == @board[4] && @board[4] == @board[5]
+    @win = true if @board[6] == @board[7] && @board[7] == @board[8]
     @win
   end
 end
