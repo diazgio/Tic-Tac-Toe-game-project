a068747c840962fa27f8405fec278f232d79e8a0
Giordano Dìaz <diazgiordano@gmail.com>
2020-08-28 10:54:07 -0500

    Puts and chomps passed to the main from logic and player classes

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,41 +1,10 @@
 # rubocop: disable Style/MultipleComparison
 
 class Logic
-  def self.turn(arr, player, player_choice)
-    valid = false
-    until valid
-      if arr[player_choice] == 'X' || arr[player_choice] == 'O'
-        puts 'Wrong postion, please choose another one'
-        return valid
-      else
-        arr[player_choice] = player.token
-        valid = true
-        break
-      end
-    end
+  def self.turn(arr)
     current_play = Board.new(arr)
     current_play.display_board
     current_play.win?
-  end
-
-  def self.reset
-    options = %w[y n yes no]
-
-    loop do
-      puts 'Do you want to play again?: (y/n)'
-      player_option = gets.chomp.downcase
-      if options.include? player_option
-        if player_option == 'yes' || player_option == 'y'
-          check = true
-          return check
-        else
-          check = false
-        end
-      else
-        puts 'Sorry, wrong option please enter a valid option.'
-      end
-      check
-    end
   end
 end
 
--- lib/player.rb
+++ lib/player.rb
@@ -6,24 +6,6 @@ class Player
     @name = name
     @token = token
   end
-
-  def to_s
-    "Player: #{@name}--#{@token}"
-  end
-
-  def get_choice(arg)
-    loop do
-      puts 'Please enter a number between 1 to 9: '
-      choice = gets.chomp.to_i
-      index = choice - 1
-      if arg.include? choice
-        return index
-        break
-      else
-        puts 'Wrong position, please choose another one'
-      end
-    end
-  end
 end
 
 # rubocop:enable Lint/UnreachableCode
