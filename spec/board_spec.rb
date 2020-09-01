ac42560e2447ffb580b1aa2fc1ea61efb4017d6b
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-09-01 14:42:50 -0500

    adding full, win and get_choice tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,5 +14,33 @@ describe Board do
       expect(board1.display_board).to eq("    1 | 2 | 3\n    ..|...|..\n    4 | 5 | 6\n    ..|...|..\n    7 | 8 | 9")
     end
   end
+
+  context "full" do
+    let (:board1) {Board.new}
+    it "Check if the board is full" do
+      expect(board1.full).to eq(false)
+    end
+  end
+
+  context "win?" do
+    let (:board1) {Board.new}
+    it "Checks if there is a winner by checking the patterns" do
+      expect(board1.win?).to be false
+    end
+  end
+
+  context "get_choice" do
+    let (:board1) {Board.new}
+
+    it "Gets user input if is true" do
+      input = 6
+      expect(board1.get_choice(input)).to eq(input - 1)
+    end
+
+    it "If user input is false" do
+      input = 10
+      expect(board1.get_choice(input)).to eq(false)
+    end
+  end
 end
 
