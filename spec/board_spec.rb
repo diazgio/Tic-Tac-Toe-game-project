8735613f6c7ca60eb194c8e32beb8aadadeefa5d
Sanad <s_wj_88@hotmail.com>
2020-09-02 00:59:21 +0300

    Rspec valid_move Rspec update Rspec switch_player

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/board'
+require_relative '../lib/player'
 
 describe Board do
   context "initialize" do
@@ -40,6 +41,35 @@ describe Board do
     it "If user input is false" do
       input = 10
       expect(board1.get_choice(input)).to eq(false)
+    end
+  end
+
+  context "valid_move" do
+    let (:board1) {Board.new}
+    it "check if the board space not available" do
+      board1.instance_variable_set("@board", ["O", 2, "X", 4, 5, 6, 7, 8, 9] )
+      expect(board1.valid_move(2)).to eq(false)
+    end
+
+    it "Check if the board space is available" do
+      player_choice = 4
+      expect(board1.valid_move(player_choice)).to eq(true)
+    end
+  end
+
+  context "update" do
+    let (:board1) {Board.new}
+    let (:token1) {Player.new("gio", "X")}
+    it "Update the player choice with the token" do
+      expect(board1.update(2, token1)).to eq(board1.instance_variable_set("@board", "X" ))
+    end
+  end
+
+  context "switch_player" do
+    let (:board1) {Board.new}
+    it "Change to the next player" do
+      switch_player = [0, 1]
+      expect(board1.switch_player(0, 1)).to eq(1)
     end
   end
 end
